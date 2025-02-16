e69927c0d45077e42ed7f958e6fd643b08d3d2ed
balumaqsud <ollobergan008@mail.ru>
2025-02-16 15:38:26 +0900

    shorter

--- 1342. NumberStep.js
+++ 1342. NumberStep.js
@@ -14,3 +14,28 @@ var numberOfSteps = function (num) {
   }
   return times_count;
 };
+
+// or
+var numberOfSteps = function (num) {
+  let times_count = 0;
+  while (num != 0) {
+    if (num % 2 === 0) {
+      num = num / 2;
+      times_count++;
+    } else {
+      num = num - 1;
+      times_count++;
+    }
+  }
+  return times_count;
+};
+
+// shorter
+var numberOfSteps = function (num) {
+  let times_count = 0;
+  while (num != 0) {
+    num % 2 === 0 ? (num /= 2) : num--;
+    times_count++;
+  }
+  return times_count;
+};
